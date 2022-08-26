3b2c837463d46dcba1472a167c6db08f8878d55d
sandhyasipan <103938932+sandhyasipan@users.noreply.github.com>
2022-08-26 22:13:06 +0530

    Update Maxnumber.java

--- Maxnumber.java
+++ Maxnumber.java
@@ -6,13 +6,13 @@ public class Maxnumber {
         X max = a;
 
         if (b.compareTo(max) > 0)
-            max = b; // y is the largest so far
+            max = b;
 
         if (c.compareTo(max) > 0)
-            max = c; // z is the largest
+            max = c;
 
-        return max; // returns the largest object
-    } // end method maximum
+        return max; 
+    } 
 
     public static void main(String[] args) {
         System.out.printf("Maximum of %d, %d and %d is %d\n\n", 10, 20, 30, maximum(10, 20, 30));
